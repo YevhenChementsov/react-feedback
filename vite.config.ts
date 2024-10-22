1b205e5c1b60562cd49dfb4fa8f29aa75ff56250
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-10-22 12:47:08 +0300

    Add theme,favicon & default import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+import { resolve } from 'node:path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react-components/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: resolve(__dirname, 'src/components'),
+      constants: resolve(__dirname, 'src/constants'),
+      helpers: resolve(__dirname, 'src/helpers'),
+    },
+  },
 });
