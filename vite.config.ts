a77804ef4404b05fae344c1fae53808404e3d3de
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-10-23 19:47:08 +0300

    Fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-components/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-feedback/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
